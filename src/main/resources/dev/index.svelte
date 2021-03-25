b0c87b39d67e7898585d47907562691a0fcdca15
Ethan Hampton <ethohampton@gmail.com>
2021-03-25 13:00:58 -0700

    Update error stuff

--- src/main/resources/dev/index.svelte
+++ src/main/resources/dev/index.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import "@shoelace-style/shoelace/dist/themes/base.css";
+  import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
   import "@shoelace-style/shoelace/dist/components/button/button.js";
   import "@shoelace-style/shoelace/dist/components/progress-bar/progress-bar.js";
+  import "@shoelace-style/shoelace/dist/components/alert/alert.js";
+  import "@shoelace-style/shoelace/dist/components/icon/icon.js";
   import wretch from "wretch";
   import cookies from "cookiesjs";
   import { onMount } from "svelte";
@@ -13,7 +16,6 @@
   let loading = false;
 
   let errorToast: any;
-  let error = false;
 
   function results() {
     loading = true;
@@ -31,7 +33,6 @@
       .catch(function (error) {
         // handle error
         console.log(error);
-        error = true;
         errorToast.toast();
       })
       .then(function () {
@@ -47,24 +48,32 @@
         .post(answer)
         .res((response) => {
           results(); //call results to display results after voting
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+          errorToast.toast();
+        })
+        .then(function () {
+          // always executed
+          loading = false;
         });
     };
   }
 
   onMount(() => {
+    setBasePath("/shoelace");
     if (cookies("submitted") === true) {
       results();
     }
   });
 </script>
 
-{#if error}
   <sl-alert type="danger" duration="5000" closable bind:this={errorToast}>
     <sl-icon slot="icon" name="exclamation-octagon" />
     <strong>There was an error</strong><br />
     There seems to be an issue, try again later
   </sl-alert>
-{/if}
 
 <div class="overall">
   {#if showingResults}
@@ -84,6 +93,7 @@
     <sl-button on:click={vote("no")} disabled={loading}>No</sl-button>
     <br />
     <sl-button on:click={results} {loading}>See Results</sl-button>
+    <!--<button on:click={errorToast.toast()}>test</button>-->
   {/if}
 </div>
 
